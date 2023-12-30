3e2140cba35b478a2756288cc60e6e2546db68f5
Ana Skarica <ana.skarica.as@gmail.com>
2023-12-30 11:02:11 +0100

    Add responsive footer

---
+++ frontend/src/components/user/molecules/Footer/Footer.tsx
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+import SocialMediaLinks from "../../../atoms/SocialMediaLinks/SocialMediaLinks";
+import logo from "../../../../assets/logo-light.png";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-list">
+        <div className="footer-list-item">
+          <div className="footer-list-item-title">BRZE POVEZNICE</div>
+          <Link to="">
+            <div className="footer-list-item-content">Destinacije</div>
+          </Link>
+          <Link to="">
+            <div className="footer-list-item-content">Savjeti</div>
+          </Link>
+          <Link to="">
+            <div className="footer-list-item-content">Aviokarte</div>
+          </Link>
+        </div>
+        <div className="footer-list-item">
+          <div className="footer-list-item-title">O NAMA</div>
+          <Link to="">
+            <div className="footer-list-item-content">Priča o nama</div>
+          </Link>
+          <Link to="">
+            <div className="footer-list-item-content">Radite s nama</div>
+          </Link>
+        </div>
+        <div className="footer-list-item">
+          <div className="footer-list-item-title">PRAVILA</div>
+          <Link to="">
+            <div className="footer-list-item-content">Uvjeti korištenja</div>
+          </Link>
+          <Link to="">
+            <div className="footer-list-item-content">Zaštita privatnosti</div>
+          </Link>
+        </div>
+        <div className="footer-list-item">
+          <div className="footer-list-item-title">KONTAKT</div>
+          <div className="footer-list-item-content">travem@gmail.com</div>
+          <span className="footer-list-item-links">
+            <SocialMediaLinks />
+          </span>
+        </div>
+      </div>
+      <div className="footer-logo">
+        <img src={logo} alt="fosleen-logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/components/user/templates/UserViewLayout.tsx
+++ frontend/src/components/user/templates/UserViewLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "../molecules/Header";
+import Footer from "../molecules/Footer";
 
 const UserViewLayout = () => {
   return (
     <div style={{ minHeight: "100vh" }}>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };

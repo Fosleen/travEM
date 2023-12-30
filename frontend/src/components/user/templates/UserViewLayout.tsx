76e7a1c093d9c52f029bdfd00cb0a7bbd6144c2b
Ana Skarica <ana.skarica.as@gmail.com>
2023-12-30 17:52:05 +0100

    Add layout max width and paddings, except on homepage

--- frontend/src/components/user/templates/UserViewLayout.tsx
+++ frontend/src/components/user/templates/UserViewLayout.tsx
@@ -1,14 +1,22 @@
 import { Outlet } from "react-router";
+import { useLocation } from "react-router-dom";
 import Header from "../molecules/Header";
 import Footer from "../molecules/Footer";
 import Newsletter from "../molecules/Newsletter";
 import "./UserViewLayout.scss";
 
 const UserViewLayout = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <main className="user-view-layout-container">
       <Header />
-      <div className="user-view-layout-page">
+      <div
+        className={`user-view-layout-page ${
+          isHomePage == false && "max-width"
+        }`}
+      >
         <Outlet />
       </div>
       <Newsletter />

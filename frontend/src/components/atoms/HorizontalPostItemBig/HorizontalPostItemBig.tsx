13fe99576e02344a5e6132fda654748dd3de9cd8
Martin Friščić <martin.friscic6@gmail.com>
2023-12-31 12:37:19 +0100

    Add link to horizontalpostitembig component

--- frontend/src/components/atoms/HorizontalPostItemBig/HorizontalPostItemBig.tsx
+++ frontend/src/components/atoms/HorizontalPostItemBig/HorizontalPostItemBig.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import "./HorizontalPostItemBig.scss";
 import postImage from "../../../assets/post-image.jpg";
+import { Link } from "react-router-dom";
 
 interface HorizontalPostItemBigProps {
   stretched?: boolean;
@@ -10,22 +11,24 @@ const HorizontalPostItemBig: FC<HorizontalPostItemBigProps> = ({
   stretched,
 }) => {
   return (
-    <div
-      className={`horizontal-post-item-big-container ${
-        stretched ? "stretched" : ""
-      }`}
-    >
-      <div className="horizontal-post-item-big-image-container">
-        <img src={postImage} alt="post-image" />
-      </div>
-      <div className="horizontal-post-item-big-text-container">
-        <h4>Kako ja letim za 5E</h4>
-        <div className="horizontal-post-item-big-inner-text-container">
-          <p>Ovo morate znati prije slijedećeg putovanja</p>
-          {!stretched && <p>5.5.2025.</p>}
+    <Link to="/">
+      <div
+        className={`horizontal-post-item-big-container ${
+          stretched ? "stretched" : ""
+        }`}
+      >
+        <div className="horizontal-post-item-big-image-container">
+          <img src={postImage} alt="post-image" />
+        </div>
+        <div className="horizontal-post-item-big-text-container">
+          <h4>Kako ja letim za 5E</h4>
+          <div className="horizontal-post-item-big-inner-text-container">
+            <p>Ovo morate znati prije slijedećeg putovanja</p>
+            {!stretched && <p>5.5.2025.</p>}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 

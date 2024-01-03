0399826ab353e5778d03fad452cea63b2e388061
Ana Skarica <ana.skarica.as@gmail.com>
2024-01-03 23:45:41 +0100

    Add blog stats components on homepage

--- frontend/src/Pages/UserViewPages/Homepage/Homepage.tsx
+++ frontend/src/Pages/UserViewPages/Homepage/Homepage.tsx
@@ -3,6 +3,7 @@ import HomepageHero from "../../../components/user/molecules/HomepageHero";
 import FavoritePosts from "../../../components/user/molecules/FavoritePosts";
 import HomepageBanner from "../../../components/user/molecules/HomepageBanner";
 import OtherPosts from "../../../components/user/molecules/OtherPosts";
+import BlogStats from "../../../components/user/molecules/BlogStats";
 
 const Homepage = () => {
   return (
@@ -10,6 +11,7 @@ const Homepage = () => {
       <HomepageHero />
       <FavoritePosts />
       <HomepageBanner />
+      <BlogStats />
       <OtherPosts />
     </div>
   );

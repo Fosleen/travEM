46d47237797db257ac13cb93a0f8fba08e46521f
Ana Skarica <ana.skarica.as@gmail.com>
2024-01-04 14:03:42 +0100

    Add DestinationPosts component to place page

--- frontend/src/Pages/UserViewPages/DestinationPlace/DestinationPlace.tsx
+++ frontend/src/Pages/UserViewPages/DestinationPlace/DestinationPlace.tsx
@@ -1,4 +1,5 @@
 import DestinationHero from "../../../components/user/molecules/DestinationHero";
+import DestinationPosts from "../../../components/user/molecules/DestinationPosts";
 import DestinationVideos from "../../../components/user/molecules/DestinationVideos";
 import RecommendedPosts from "../../../components/user/molecules/RecommendedPosts";
 import "./DestinationPlace.scss";
@@ -7,6 +8,12 @@ const DestinationPlace = () => {
   return (
     <div className="destination-place-page-container">
       <DestinationHero />
+      <div className="destination-place-posts-container">
+        <h2>Saznajte više</h2>
+        <div className="destination-place-posts">
+          <DestinationPosts />
+        </div>
+      </div>
       <div className="destination-place-videos-container">
         <h2>Vlogovi i video putopisi</h2>
         <div className="destination-place-videos">

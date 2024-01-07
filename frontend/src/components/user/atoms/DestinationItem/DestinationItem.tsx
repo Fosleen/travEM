fb6f5e826d38835eb5901194272565b1ee030fa1
Ana Skarica <ana.skarica.as@gmail.com>
2024-01-07 13:47:28 +0100

    Add destination item version with flag left

--- frontend/src/components/user/atoms/DestinationItem/DestinationItem.tsx
+++ frontend/src/components/user/atoms/DestinationItem/DestinationItem.tsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 import icon from "../../../../assets/images/building-icon.png";
+import flag from "../../../../assets/images/croatian-flag.png";
 import "./DestinationItem.scss";
 
-const DestinationItem = ({ mapItem = false }) => {
+const DestinationItem = ({ mapItem = false, filterMenuItem = false }) => {
   return (
     <Link
       to="/"
-      className={`destination-item-container ${mapItem && "has-icon"}`}
+      className={`destination-item-container ${
+        (mapItem || filterMenuItem) && "has-icon flag"
+      }`}
     >
+      {filterMenuItem && <img src={flag} alt="destination-image" />}
       <p>London</p>
       {mapItem && <img src={icon} alt="destination-image" />}
     </Link>

3c380387c7db70653c7d54c945034de208c98e17
Martin Friščić <martin.friscic6@gmail.com>
2024-01-07 23:33:18 +0100

    Add cursor styling on map

--- frontend/src/components/organisms/Map/Map.tsx
+++ frontend/src/components/organisms/Map/Map.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import Map, {
   Marker,
   NavigationControl,
@@ -9,80 +9,20 @@ import Map, {
 import "mapbox-gl/dist/mapbox-gl.css";
 import Pin from "../../atoms/Pin/Pin";
 import "./Map.scss";
+import CITIES from "./cities";
 
 const MapNew = () => {
-  const CITIES = [
-    {
-      city: "Paris",
-      state: "Île-de-France",
-      latitude: 48.8566,
-      longitude: 2.3522,
-      image: "url_to_image_for_paris",
-    },
-    {
-      city: "London",
-      state: "England",
-      latitude: 51.509865,
-      longitude: -0.118092,
-      image: "url_to_image_for_london",
-    },
-    {
-      city: "Berlin",
-      state: "Berlin",
-      latitude: 52.52,
-      longitude: 13.405,
-      image: "url_to_image_for_berlin",
-    },
-    {
-      city: "Rome",
-      state: "Lazio",
-      latitude: 41.9028,
-      longitude: 12.4964,
-      image: "url_to_image_for_rome",
-    },
-    {
-      city: "Barcelona",
-      state: "Catalonia",
-      latitude: 41.3851,
-      longitude: 2.1734,
-      image: "url_to_image_for_barcelona",
-    },
-    {
-      city: "Vienna",
-      state: "Vienna",
-      latitude: 48.8566,
-      longitude: 16.3522,
-      image: "url_to_image_for_vienna",
-    },
-    {
-      city: "Athens",
-      state: "Attica",
-      latitude: 37.9838,
-      longitude: 23.7275,
-      image: "url_to_image_for_athens",
-    },
-    {
-      city: "Prague",
-      state: "Prague",
-      latitude: 50.0755,
-      longitude: 14.4378,
-      image: "url_to_image_for_prague",
-    },
-    {
-      city: "Stockholm",
-      state: "Stockholm",
-      latitude: 59.3293,
-      longitude: 18.0686,
-      image: "url_to_image_for_stockholm",
-    },
-    {
-      city: "Lisbon",
-      state: "Lisbon",
-      latitude: 38.7223,
-      longitude: -9.1393,
-      image: "url_to_image_for_lisbon",
-    },
-  ];
+  const [hoveredCountry, setHoveredCountry] = useState(null);
+
+  const [cursor, setCursor] = useState<string>("auto");
+
+  const onClick = useCallback((event) => {
+    const feature = event.features && event.features[0];
+
+    if (feature) {
+      window.alert(`Clicked layer ${feature.layer.id}`); // eslint-disable-line no-alert
+    }
+  }, []);
 
   const pins = useMemo(
     () =>
@@ -98,19 +38,22 @@ const MapNew = () => {
       )),
     []
   );
+
   return (
     <div className="map-parent-wrapper">
       <Map
         initialViewState={{
-          latitude: 51.1657, // Latitude for the center of Europe
-          longitude: 10.4515, // Longitude for the center of Europe
-          zoom: 3, // Adjust the zoom level as needed
+          latitude: 51.1657,
+          longitude: 10.4515,
+          zoom: 3,
           bearing: 0,
           pitch: 0,
         }}
         mapStyle="mapbox://styles/mapbox/navigation-day-v1"
         mapboxAccessToken="pk.eyJ1IjoidHJhdmVtIiwiYSI6ImNscjN6b21hZzFwYzYyaW15NHR1MGF2c3YifQ.i0zgKxn9bundlWQQasv_rA"
         scrollZoom={false}
+        onClick={onClick}
+        cursor={cursor}
       >
         <GeolocateControl position="top-left" />
         <FullscreenControl position="top-left" />

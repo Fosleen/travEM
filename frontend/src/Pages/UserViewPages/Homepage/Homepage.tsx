cd736fb7590aab22fa3cba380b2dfa3a1428826d
Martin Friščić <martin.friscic6@gmail.com>
2024-01-07 23:05:49 +0100

    Add first version of new map with markers

--- frontend/src/Pages/UserViewPages/Homepage/Homepage.tsx
+++ frontend/src/Pages/UserViewPages/Homepage/Homepage.tsx
@@ -4,8 +4,8 @@ import FavoritePosts from "../../../components/user/molecules/FavoritePosts";
 import HomepageBanner from "../../../components/user/molecules/HomepageBanner";
 import OtherPosts from "../../../components/user/molecules/OtherPosts";
 import BlogStats from "../../../components/user/molecules/BlogStats";
-import Map from "../../../components/organisms/Map/Map";
 import RecommendedaMapDestinations from "../../../components/user/molecules/RecommendedaMapDestinations";
+import MapNew from "../../../components/organisms/Map/Map";
 
 const Homepage = () => {
   return (
@@ -14,7 +14,7 @@ const Homepage = () => {
       <FavoritePosts />
       <HomepageBanner />
       <RecommendedaMapDestinations />
-      <Map />
+      <MapNew />
       <BlogStats />
       <OtherPosts />
     </div>
--- frontend/src/components/organisms/Map/Map.tsx
+++ frontend/src/components/organisms/Map/Map.tsx
@@ -1,73 +1,125 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useMemo } from "react";
+import Map, {
+  Marker,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl,
+} from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Pin from "../../atoms/Pin/Pin";
 import "./Map.scss";
-import {
-  ComposableMap,
-  ZoomableGroup,
-  Geographies,
-  Geography,
-} from "react-simple-maps";
-import WorldCountries from "../Map/features.json";
 
-const Map = () => {
-  const [selectedCountry, setSelectedCountry] = useState(null);
-  const navigate = useNavigate();
+const MapNew = () => {
+  const CITIES = [
+    {
+      city: "Paris",
+      state: "Île-de-France",
+      latitude: 48.8566,
+      longitude: 2.3522,
+      image: "url_to_image_for_paris",
+    },
+    {
+      city: "London",
+      state: "England",
+      latitude: 51.509865,
+      longitude: -0.118092,
+      image: "url_to_image_for_london",
+    },
+    {
+      city: "Berlin",
+      state: "Berlin",
+      latitude: 52.52,
+      longitude: 13.405,
+      image: "url_to_image_for_berlin",
+    },
+    {
+      city: "Rome",
+      state: "Lazio",
+      latitude: 41.9028,
+      longitude: 12.4964,
+      image: "url_to_image_for_rome",
+    },
+    {
+      city: "Barcelona",
+      state: "Catalonia",
+      latitude: 41.3851,
+      longitude: 2.1734,
+      image: "url_to_image_for_barcelona",
+    },
+    {
+      city: "Vienna",
+      state: "Vienna",
+      latitude: 48.8566,
+      longitude: 16.3522,
+      image: "url_to_image_for_vienna",
+    },
+    {
+      city: "Athens",
+      state: "Attica",
+      latitude: 37.9838,
+      longitude: 23.7275,
+      image: "url_to_image_for_athens",
+    },
+    {
+      city: "Prague",
+      state: "Prague",
+      latitude: 50.0755,
+      longitude: 14.4378,
+      image: "url_to_image_for_prague",
+    },
+    {
+      city: "Stockholm",
+      state: "Stockholm",
+      latitude: 59.3293,
+      longitude: 18.0686,
+      image: "url_to_image_for_stockholm",
+    },
+    {
+      city: "Lisbon",
+      state: "Lisbon",
+      latitude: 38.7223,
+      longitude: -9.1393,
+      image: "url_to_image_for_lisbon",
+    },
+  ];
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignoreS
-
-  const handleCountryClick = (geo) => {
-    const countryName = geo.properties.name;
-    setSelectedCountry(countryName);
-    navigate(`/${countryName}`);
-  };
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignoreS
-  const handleCountryHover = (geo, isHovered) => {
-    setSelectedCountry(isHovered ? geo.properties.name : null);
-  };
-
-  const coloredCountries = ["Germany", "France", "Spain", "Croatia"];
-
+  const pins = useMemo(
+    () =>
+      CITIES.map((city, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={city.longitude}
+          latitude={city.latitude}
+          anchor="bottom"
+        >
+          <Pin />
+        </Marker>
+      )),
+    []
+  );
   return (
     <div className="map-parent-wrapper">
-      <ComposableMap>
-        <ZoomableGroup center={[15, 50]} zoom={6} minZoom={2} maxZoom={8}>
-          <rect width="100%" height="100%" fill="#73b6e6" />
-          <Geographies geography={WorldCountries}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={
-                    selectedCountry === geo.properties.name
-                      ? "#2BAC82" // Color for the selected country
-                      : coloredCountries.includes(geo.properties.name)
-                      ? "#D2EB64" // Color for specific countries
-                      : "white" // Default color for other countries
-                  }
-                  stroke="grey"
-                  strokeOpacity={0.05}
-                  onMouseEnter={() => handleCountryHover(geo, true)}
-                  onMouseLeave={() => handleCountryHover(geo, false)}
-                  onClick={() => handleCountryClick(geo)}
-                  style={{
-                    default: { outline: "none" },
-                    hover: { outline: "none" },
-                    pressed: { outline: "none" },
-                  }}
-                />
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
-        {/* <Marker coordinates={[45.80881, 15.9304]}>
-          <PushPin color="red" size={24} weight="fill" />   markers dont work as they should
-        </Marker> */}
-      </ComposableMap>
+      <Map
+        initialViewState={{
+          latitude: 51.1657, // Latitude for the center of Europe
+          longitude: 10.4515, // Longitude for the center of Europe
+          zoom: 3, // Adjust the zoom level as needed
+          bearing: 0,
+          pitch: 0,
+        }}
+        mapStyle="mapbox://styles/mapbox/navigation-day-v1"
+        mapboxAccessToken="pk.eyJ1IjoidHJhdmVtIiwiYSI6ImNscjN6b21hZzFwYzYyaW15NHR1MGF2c3YifQ.i0zgKxn9bundlWQQasv_rA"
+      >
+        <GeolocateControl position="top-left" />
+        <FullscreenControl position="top-left" />
+        <NavigationControl position="top-left" />
+        <ScaleControl />
+
+        {pins}
+      </Map>
     </div>
   );
 };
 
-export default Map;
+export default MapNew;

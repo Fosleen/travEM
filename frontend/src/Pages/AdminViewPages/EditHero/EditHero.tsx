355f867f3c8d00e1b1bcf860970446cf50cb1662
Ana Škarica <ana.skarica.as@gmail.com>
2024-01-14 15:44:49 +0100

    Add connect edit homepage hero with backend

--- frontend/src/Pages/AdminViewPages/EditHero/EditHero.tsx
+++ frontend/src/Pages/AdminViewPages/EditHero/EditHero.tsx
@@ -2,20 +2,46 @@ import { useEffect, useState } from "react";
 import Button from "../../../components/atoms/Button";
 import "./EditHero.scss";
 import { HomepageData } from "../../../common/types";
-import { getHomepage } from "../../../api/homepage";
+import { getHomepage, updateHeroImage } from "../../../api/homepage";
 import { PencilSimpleLine } from "@phosphor-icons/react";
+import Input from "../../../components/atoms/Input";
+import { useNavigate } from "react-router-dom";
+import {
+  notifyFailure,
+  notifySuccess,
+} from "../../../components/atoms/Toast/Toast";
 
 const EditHero = () => {
   const [homepageContent, setHomepageContent] = useState<HomepageData | null>(
     null
   );
+  const navigate = useNavigate();
+  const [isUrlShown, setIsUrlShown] = useState(false);
+  const [url, setUrl] = useState("");
 
   const fetchData = async () => {
     try {
       const content = await getHomepage();
       setHomepageContent(content);
+      setUrl(content.hero_image_url);
     } catch (error) {
       console.error("Error occured while fetching homepage data:", error);
+    }
+  };
+
+  const handleCancel = () => {
+    navigate("/admin/sadržaj");
+  };
+
+  const handleSave = async () => {
+    try {
+      const updatedContent = await updateHeroImage(url);
+      setHomepageContent(updatedContent);
+      setIsUrlShown(false);
+      notifySuccess("Uspješno ažurirano!");
+    } catch (error) {
+      console.log(error);
+      notifyFailure("Došlo je do greške. Pokušajte ponovo.");
     }
   };
 
@@ -27,16 +53,44 @@ const EditHero = () => {
     <div className="edit-hero-container">
       <h2>Uredi naslovnu fotografiju</h2>
       <div className="edit-hero-image-container">
-        <img src={homepageContent?.hero_image_url} alt="hero-image" />
-        <Button edit>
-          <PencilSimpleLine size={32} />
-        </Button>
+        <img
+          src={homepageContent?.hero_image_url}
+          alt="greska-s-prikazom-ili-vrstom-slike"
+        />
+        {!isUrlShown && (
+          <Button
+            edit
+            onClick={() => {
+              setIsUrlShown(true);
+            }}
+          >
+            <PencilSimpleLine size={32} />
+          </Button>
+        )}
       </div>
-      <p>*preporuča se slika u omjeru 16:9</p>
-      <div className="edit-hero-buttons">
-        <Button adminPrimary>spremi promjene</Button>
-        <Button white>odustani</Button>
-      </div>
+      {isUrlShown && (
+        <div className="edit-hero-input-container">
+          <Input
+            placeholder="Unesi URL nove slike..."
+            name="url"
+            value={url}
+            onChange={(e) => {
+              setUrl(e.target.value);
+            }}
+          />
+        </div>
+      )}
+      <p>* preporuča se slika u omjeru 16:9</p>
+      {isUrlShown && (
+        <div className="edit-hero-buttons">
+          <Button adminPrimary onClick={handleSave}>
+            spremi promjene
+          </Button>
+          <Button white onClick={handleCancel}>
+            odustani
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/api/homepage.ts
+++ frontend/src/api/homepage.ts
@@ -10,3 +10,24 @@ export async function getHomepage() {
   }
   return data;
 }
+
+export async function updateHeroImage(url: string) {
+  const response = await fetch(`${apiUrl}/homepage`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "PATCH",
+    body: JSON.stringify({
+      hero_image_url: url,
+    }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    console.log(data.error);
+    return data.error;
+  }
+  return data;
+}

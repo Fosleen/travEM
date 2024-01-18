2dde0d35e84b6b4d2e110846c30e501e9560b407
Ana Škarica <ana.skarica.as@gmail.com>
2024-01-18 11:02:37 +0100

    Add section on add article form UI

--- frontend/src/Pages/AdminViewPages/AddArticle/AddArticle.tsx
+++ frontend/src/Pages/AdminViewPages/AddArticle/AddArticle.tsx
@@ -8,11 +8,13 @@ import { useEffect, useState } from "react";
 import { getArticleTypes } from "../../../api/articleTypes";
 import Dropdown from "../../../components/atoms/Dropdown";
 import { getVisitedCountries } from "../../../api/map";
+import { Plus, TrashSimple } from "@phosphor-icons/react";
 import {
   ArticleType,
   MapCountriesData,
   PlacesData,
 } from "../../../common/types";
+import image from "../../../assets/images/post-image.jpg";
 
 const AddArticle = () => {
   const navigate = useNavigate();
@@ -51,6 +53,14 @@ const AddArticle = () => {
     setSelectedPlaceId(selectedValue);
   };
 
+  const handleDeleteSection = () => {
+    console.log("delete section");
+  };
+
+  const handleDeleteImage = () => {
+    console.log("remove image");
+  };
+
   const fetchData = async () => {
     try {
       const articleTypesData = await getArticleTypes();
@@ -80,7 +90,7 @@ const AddArticle = () => {
           validationSchema={ValidationSchema}
           onSubmit={handleSave}
         >
-          <Form>
+          <Form className="add-article-form">
             <div className="add-article-inputs">
               <Field
                 name="article_title"
@@ -148,7 +158,85 @@ const AddArticle = () => {
                 )}
               </div>
             </div>
-            <div className="add-article-image-container"></div>
+            <div className="add-article-image-container">
+              <div className="add-article-item">
+                <Plus size={32} color="#616161" weight="bold" />
+              </div>
+            </div>
+            <p>* preporuča se slika u omjeru 16:9</p>
+
+            <div className="add-article-sections-container">
+              <h6>Odlomci</h6>
+              <fieldset className="add-article-section">
+                <legend>Odlomak 1</legend>
+                <div className="add-article-section-top">
+                  <div className="add-article-section-top-item">
+                    <Field
+                      name="section_subtitle"
+                      type="text"
+                      as={Input}
+                      label="Podnaslov odlomka (opcionalno)"
+                      placeholder="Unesi podnaslov odlomka..."
+                    />
+                  </div>
+                  <div className="add-article-section-top-item">
+                    <Dropdown
+                      hardcodedValue={"Odaberi..."}
+                      options={articleTypes}
+                      onChange={handleArticleTypeChange}
+                      value={selectedArticleTypeId}
+                      label="Vrsta ikone *"
+                    />
+                  </div>
+                </div>
+                <Field
+                  name="section_subtitle"
+                  type="text"
+                  as={Input}
+                  label="Tekst odlomka *"
+                  placeholder="Unesi tekst odlomka..."
+                />
+                <div className="add-article-section-bottom">
+                  <Field
+                    name="section_subtitle"
+                    type="text"
+                    as={Input}
+                    label="Naslov poveznice (opcionalno)"
+                    placeholder="Unesi naslov poveznice..."
+                  />
+                  <Field
+                    name="section_subtitle"
+                    type="text"
+                    as={Input}
+                    label="Poveznica (opcionalno)"
+                    placeholder="Unesi link poveznice..."
+                  />
+                </div>
+                <div className="add-article-bottom-container">
+                  <div className="add-article-image-container">
+                    <div
+                      className="add-article-image"
+                      onClick={handleDeleteImage}
+                    >
+                      <div className="add-article-image-remove-icon">
+                        <TrashSimple size={32} color="#e70101" weight="fill" />
+                      </div>
+                      <img src={image} alt="selected-image" />
+                    </div>
+                    <div className="add-article-item">
+                      <Plus size={32} color="#616161" weight="bold" />
+                    </div>
+                  </div>
+                  <Button type="button" red onClick={handleDeleteSection}>
+                    izbriši odlomak
+                  </Button>
+                </div>
+              </fieldset>
+              <fieldset className="add-article-section">
+                <legend>Odlomak 2</legend>
+                <h6>nekaj</h6>
+              </fieldset>
+            </div>
             <div className="add-article-buttons">
               <Button type="submit" adminPrimary>
                 objavi članak

df5f1109b7f3f50bf2e84fa60e2bf1d40d96c064
Ana Škarica <ana.skarica.as@gmail.com>
2024-01-24 13:42:31 +0100

    Connect formik with section form normal inputs

--- frontend/src/Pages/AdminViewPages/AddArticle/AddArticle.tsx
+++ frontend/src/Pages/AdminViewPages/AddArticle/AddArticle.tsx
@@ -1,4 +1,4 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, FieldArray, Form, Formik } from "formik";
 import "./AddArticle.scss";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
@@ -51,11 +51,14 @@ const AddArticle = () => {
   });
 
   const handleSave = async (values) => {
+    console.log(
+      "zakej se ovo opet ne poziva haaaa - jer nisi napisala naslov clanka koji je obavezan!!!!"
+    );
     console.log(values);
-
-    const dateString = new Date().toJSON().slice(0, 10);
+    /*
+    const dateString = new Date().toJSON().slice(0, 10); 
     const todaysDate = new Date(dateString);
-
+   
     const response = await addArticle(
       values.article_title,
       values.article_subtitle,
@@ -66,10 +69,10 @@ const AddArticle = () => {
       parseInt(values.article_place),
       "https://live.staticflickr.com/65535/53459367725_3c3e143b26_b.jpg", // main image hardcoded
       1, // hardcoded user id
-      todaysDate,
+      todaysDate
     );
 
-    console.log(response);
+    console.log(response);*/
   };
 
   const handleCancel = () => {
@@ -94,8 +97,19 @@ const AddArticle = () => {
     setSelectedSectionIcon(selectedValue);
   };
 
-  const handleDeleteSection = () => {
-    console.log("delete section");
+  const handleDeleteSection = (arrayHelpers, index) => {
+    arrayHelpers.remove(index);
+  };
+
+  const handleAddSection = (arrayHelpers) => {
+    arrayHelpers.push({
+      section_subtitle: "",
+      section_text: "",
+      section_url_title: "",
+      section_url_link: "",
+      section_icon: "",
+      order: 1,
+    });
   };
 
   const handleDeleteImage = () => {
@@ -142,6 +156,16 @@ const AddArticle = () => {
             article_type: "",
             article_country: "",
             article_place: "",
+            sections: [
+              {
+                section_subtitle: "",
+                section_text: "",
+                section_url_title: "",
+                section_url_link: "",
+                section_icon: "",
+                order: 1,
+              },
+            ],
           }}
           validationSchema={ValidationSchema}
           onSubmit={handleSave}
@@ -226,78 +250,116 @@ const AddArticle = () => {
               </div>
               <p>* preporuča se slika u omjeru 16:9</p>
 
-              <div className="add-article-sections-container">
+              <div>
                 <h6>Odlomci</h6>
-                <fieldset className="add-article-section">
-                  <legend>Odlomak 1</legend>
-                  <div className="add-article-section-top">
-                    <div className="add-article-section-top-item">
-                      <Field
-                        name="section_subtitle"
-                        type="text"
-                        as={Input}
-                        label="Podnaslov odlomka (opcionalno)"
-                        placeholder="Unesi podnaslov odlomka..."
-                      />
-                    </div>
-                    <div className="add-article-section-top-item">
-                      <AdvancedDropdown
-                        hardcodedValue="Odaberi..."
-                        label="Vrsta ikone *"
-                        options={sectionIcons}
-                        value={selectedArticleTypeId}
-                        onChange={handleSectionIconChange}
-                        filter={false}
-                        images={true}
-                      />
-                    </div>
-                  </div>
-                  <Field
-                    name="section_subtitle"
-                    type="text"
-                    as={Input}
-                    label="Tekst odlomka *"
-                    placeholder="Unesi tekst odlomka..."
-                  />
-                  <div className="add-article-section-bottom">
-                    <Field
-                      name="section_subtitle"
-                      type="text"
-                      as={Input}
-                      label="Naslov poveznice (opcionalno)"
-                      placeholder="Unesi naslov poveznice..."
-                    />
-                    <Field
-                      name="section_subtitle"
-                      type="text"
-                      as={Input}
-                      label="Poveznica (opcionalno)"
-                      placeholder="Unesi link poveznice..."
-                    />
-                  </div>
-                  <div className="add-article-bottom-container">
-                    <div className="add-article-images-container">
-                      <div
-                        className="add-article-image"
-                        onClick={handleDeleteImage}
-                      >
-                        <div className="add-article-image-remove-icon">
-                          <X size={32} color="#e70101" weight="bold" />
-                        </div>
-                        <img src={image} alt="selected-image" />
+                <FieldArray
+                  name="sections"
+                  render={(arrayHelpers) => {
+                    const sections = values.sections;
+
+                    return (
+                      <div className="add-article-sections-container">
+                        {sections && sections.length > 0
+                          ? sections.map((section, index) => (
+                              <fieldset
+                                key={index}
+                                className="add-article-section"
+                              >
+                                <legend>Odlomak {index + 1}</legend>
+                                <div className="add-article-section-top">
+                                  <div className="add-article-section-top-item">
+                                    <Field
+                                      name={`sections.${index}.section_subtitle`}
+                                      type="text"
+                                      as={Input}
+                                      label="Podnaslov odlomka (opcionalno)"
+                                      placeholder="Unesi podnaslov odlomka..."
+                                    />
+                                  </div>
+                                  <div className="add-article-section-top-item">
+                                    <AdvancedDropdown
+                                      hardcodedValue="Odaberi..."
+                                      label="Vrsta ikone *"
+                                      name={`sections.${index}.section_icon`} // TODO connect this with formik
+                                      options={sectionIcons}
+                                      onChange={handleSectionIconChange}
+                                      filter={false}
+                                      images={true}
+                                    />
+                                  </div>
+                                </div>
+                                <Field
+                                  type="text"
+                                  as={Input}
+                                  name={`sections.${index}.section_text`}
+                                  label="Tekst odlomka *"
+                                  placeholder="Unesi tekst odlomka..."
+                                />
+                                <div className="add-article-section-bottom">
+                                  <Field
+                                    type="text"
+                                    name={`sections.${index}.section_url_title`}
+                                    as={Input}
+                                    label="Naslov poveznice (opcionalno)"
+                                    placeholder="Unesi naslov poveznice..."
+                                  />
+                                  <Field
+                                    type="text"
+                                    name={`sections.${index}.section_url_link`}
+                                    as={Input}
+                                    label="Poveznica (opcionalno)"
+                                    placeholder="Unesi link poveznice..."
+                                  />
+                                </div>
+                                <div className="add-article-bottom-container">
+                                  <div className="add-article-images-container">
+                                    <div
+                                      className="add-article-image"
+                                      onClick={handleDeleteImage}
+                                    >
+                                      <div className="add-article-image-remove-icon">
+                                        <X
+                                          size={32}
+                                          color="#e70101"
+                                          weight="bold"
+                                        />
+                                      </div>
+                                      <img src={image} alt="selected-image" />
+                                    </div>
+                                    <div className="add-article-item">
+                                      <Plus
+                                        size={32}
+                                        color="#616161"
+                                        weight="bold"
+                                      />
+                                    </div>
+                                  </div>
+                                  <Button
+                                    type="button"
+                                    red
+                                    onClick={() => {
+                                      handleDeleteSection(arrayHelpers, index);
+                                    }}
+                                  >
+                                    izbriši odlomak
+                                  </Button>
+                                </div>
+                              </fieldset>
+                            ))
+                          : null}
+                        <Button
+                          type="button"
+                          primary
+                          onClick={() => {
+                            handleAddSection(arrayHelpers);
+                          }}
+                        >
+                          dodaj odlomak
+                        </Button>
                       </div>
-                      <div className="add-article-item">
-                        <Plus size={32} color="#616161" weight="bold" />
-                      </div>
-                    </div>
-                    <Button type="button" red onClick={handleDeleteSection}>
-                      izbriši odlomak
-                    </Button>
-                  </div>
-                </fieldset>
-                <Button type="button" primary onClick={handleDeleteSection}>
-                  dodaj odlomak
-                </Button>
+                    );
+                  }}
+                />
               </div>
               <div className="add-article-gallery-container">
                 <h6>Preostale fotografije na članku:</h6>

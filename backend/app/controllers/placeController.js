c342091894600a31036e969f523dd9763a36b535
Ana Skarica <ana.skarica.as@gmail.com>
2024-02-04 11:24:56 +0100

    Add endpoints for places

--- backend/app/controllers/placeController.js
+++ backend/app/controllers/placeController.js
@@ -9,6 +9,91 @@ class PlacesController {
       res.status(200).json(response);
     }
   }
+
+  async getPlaces(req, res) {
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 12;
+
+    const response = await service.getPlaces(page, pageSize);
+
+    if (response.places.length === 0) {
+      res.status(404).json({ error: "No places found" });
+    } else {
+      res.status(200).json(response);
+    }
+  }
+
+  async getPlaceById(req, res) {
+    const { id } = req.params;
+    const response = await service.getPlaceById(id);
+    if (!response || response.length == 0) {
+      res.status(404).json({ error: `No place found by id ${id}` });
+    } else {
+      res.status(200).json(response);
+    }
+  }
+
+  async getPlaceByName(req, res) {
+    const { name } = req.params;
+    const response = await service.getPlaceByName(name);
+    if (!response || response.length == 0) {
+      res.status(404).json({ error: `No place found by name ${name}` });
+    } else {
+      res.status(200).json(response);
+    }
+  }
+
+  async addPlace(req, res) {
+    const response = await service.addPlace(
+      req.body.name,
+      req.body.description,
+      req.body.main_image_url,
+      req.body.map_icon,
+      req.body.is_on_homepage_map,
+      req.body.latitude,
+      req.body.longitude,
+      req.body.country_id
+    );
+
+    console.log(response.toJSON());
+
+    if (response == undefined) {
+      res.status(500).json({ error: "Error inserting place" });
+    } else {
+      res.status(200).json(response);
+    }
+  }
+
+  async patchPlace(req, res) {
+    const response = await service.patchPlace(
+      req.params.id,
+      req.body.name,
+      req.body.description,
+      req.body.main_image_url,
+      req.body.map_icon,
+      req.body.is_on_homepage_map,
+      req.body.latitude,
+      req.body.longitude,
+      req.body.country_id
+    );
+    if (response.length == 0) {
+      res.status(500).json({ error: `Error updating place ${id}` });
+    } else {
+      res.status(200).json(response);
+    }
+  }
+
+  async deletePlaceAndArticles(req, res) {
+    const { id } = req.params;
+    const response = await service.deletePlaceAndArticles(id);
+    if (response) {
+      res.status(200).json({});
+    } else {
+      res
+        .status(500)
+        .json({ error: `Error while deleting place with id ${id}` });
+    }
+  }
 }
 
 export default new PlacesController();
--- backend/app/routes/placeRoute.js
+++ backend/app/routes/placeRoute.js
@@ -3,6 +3,25 @@ import controller from "../controllers/placeController.js";
 
 const router = new Router();
 
+// GET /api/v1/places/map
 router.get("/map", controller.getFavoritePlaces);
+
+// GET /api/v1/places?page=1&pageSize=12
+router.get("/", controller.getPlaces);
+
+// GET /api/v1/places/1
+router.get("/:id", controller.getPlaceById);
+
+// GET /api/v1/places/search/zagr
+router.get("/search/:name", controller.getPlaceByName);
+
+// POST /api/v1/places/
+router.post("/", controller.addPlace);
+
+// PATCH /api/v1/places/4
+router.patch("/:id", controller.patchPlace);
+
+// DELETE /api/v1/countries/2
+router.delete("/:id", controller.deletePlaceAndArticles);
 
 export default router;

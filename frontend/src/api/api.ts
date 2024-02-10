6381026e92a7a0f6a168cc438ce65f4a101ee582
Martin Friščić <martin.friscic6@gmail.com>
2024-02-10 17:04:33 +0100

    Add hosted backend port

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,3 @@
-export const apiUrl: string = `http://localhost:${
-  import.meta.env.VITE_PORT
+export const apiUrl: string = `http://164.90.168.222:25060/
 }/api/v1`;
 export const token: string | null = localStorage.getItem("token");

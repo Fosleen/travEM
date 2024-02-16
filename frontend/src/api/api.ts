a712ec58ea8a8dfa7db7e5c9b09389f4d42d9eff
Martin Friščić <martin.friscic6@gmail.com>
2024-02-17 00:26:30 +0100

    Url fix

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,2 +1,2 @@
-export const apiUrl: string = `https://travem-backend.com`;
+export const apiUrl: string = `https://travem-backend.com/api/v1`;
 export const token: string | null = localStorage.getItem("token");

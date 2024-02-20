39575d5703dce200d356002d2c6c0fd26589a2dd
Martin Friščić <martin.friscic6@gmail.com>
2024-02-20 14:48:45 +0100

    Return good api endpoint

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,2 +1,2 @@
-export const apiUrl: string = `http://localhost:25060/api/v1`;
+export const apiUrl: string = `https://travem-backend.com/api/v1`;
 export const token: string | null = localStorage.getItem("token");

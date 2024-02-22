0da75bb2c2210087f41d7a718922b131fdc3957f
Ana Škarica <ana.skarica.as@gmail.com>
2024-02-22 13:04:49 +0100

    Add description to article type, fix endpoint to get recommended articles

--- backend/app/models/articleType.js
+++ backend/app/models/articleType.js
@@ -3,6 +3,7 @@ export default (sequelize, DataTypes) => {
     "article_type",
     {
       name: { type: DataTypes.STRING(100), allowNull: false },
+      description: { type: DataTypes.STRING(100), allowNull: true },
     },
     { underscored: true, timestamps: false, freezeTableName: true }
   );

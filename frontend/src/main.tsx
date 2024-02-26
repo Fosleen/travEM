e300078625f92cae1418f7bcf0e778d001205e56
Ana Škarica <ana.skarica.as@gmail.com>
2024-02-26 14:39:06 +0100

    Add ga4 with page views tracking

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,15 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { PrimeReactProvider } from "primereact/api";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-P636ECTSKC");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: "Page views count",
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

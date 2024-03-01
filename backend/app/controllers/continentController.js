2576ef56749ef7ab751262338651d34c4e9aec7a
Ana Škarica <ana.skarica.as@gmail.com>
2024-03-01 17:48:56 +0100

    Add swagger docs for all continent endpoints, minor fixes

--- backend/app/controllers/continentController.js
+++ backend/app/controllers/continentController.js
@@ -2,41 +2,62 @@ import service from "../services/continentService.js";
 
 class ContinentsController {
   async getContinents(req, res) {
-    const response = await service.getContinents();
-    if (response == undefined) {
-      res.status(404).json({ error: "No continents found" });
-    } else {
-      res.status(200).json(response);
+    try {
+      const response = await service.getContinents();
+      if (response == undefined) {
+        res.status(404).json({ error: "No continents found" });
+      } else {
+        res.status(200).json(response);
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
     }
   }
 
   async getContinentById(req, res) {
-    const { id } = req.params;
-    const response = await service.getContinentById(id);
-    if (response == undefined) {
-      res.status(404).json({ error: "No continents found" });
-    } else {
-      res.status(200).json(response);
+    try {
+      const { id } = req.params;
+      const response = await service.getContinentById(id);
+      if (response == undefined) {
+        res.status(404).json({ error: "No continent found" });
+      } else {
+        res.status(200).json(response);
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
     }
   }
 
   async getContinentCountries(req, res) {
-    const { id } = req.params;
-    const response = await service.getContinentCountries(id);
-    if (response == undefined) {
-      res.status(404).json({ error: "No continent countries found" });
-    } else {
-      res.status(200).json(response);
+    try {
+      const { id } = req.params;
+      const response = await service.getContinentCountries(id);
+      if (response === "Continent not found") {
+        return res
+          .status(404)
+          .json({ error: "Continent with the provided ID doesn't exist" });
+      } else {
+        res.status(200).json(response);
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
     }
   }
 
   async getContinentPlaces(req, res) {
-    const { id } = req.params;
-    const response = await service.getContinentPlaces(id);
-    if (response == undefined) {
-      res.status(404).json({ error: "No continent places found" });
-    } else {
-      res.status(200).json(response);
+    try {
+      const { id } = req.params;
+      const response = await service.getContinentPlaces(id);
+
+      if (response === "Continent not found") {
+        return res
+          .status(404)
+          .json({ error: "Continent with the provided ID doesn't exist" });
+      } else {
+        return res.status(200).json(response);
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
     }
   }
 }

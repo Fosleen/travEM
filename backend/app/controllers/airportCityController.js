ba6d5d9bc7006918e3bf17d3940d5a89346c2ddb
Martin Friščić <martin.friscic6@gmail.com>
2024-03-03 15:00:46 +0100

    Add documents for airport cities

--- backend/app/controllers/airportCityController.js
+++ backend/app/controllers/airportCityController.js
@@ -2,11 +2,15 @@ import airportCityService from "../services/airportCityService.js";
 
 class AirportCityController {
   async getAirportCities(req, res) {
-    const response = await airportCityService.getAirportCities();
-    if (response.length == 0) {
-      res.status(404).json({ error: `No airport cities found` });
-    } else {
-      res.status(200).json(response);
+    try {
+      const response = await airportCityService.getAirportCities();
+      if (response.length == 0) {
+        res.status(404).json({ error: `No airport cities found` });
+      } else {
+        res.status(200).json(response);
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
     }
   }
 }

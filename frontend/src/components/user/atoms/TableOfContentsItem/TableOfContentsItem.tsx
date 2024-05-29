5af24ae2731c42747aeaaeca1f9ad7cad9f0ccd9
Fr1k1 <martin.friscic6@gmail.com>
2024-05-29 13:07:31 +0200

    Improve scrolling on article page

--- frontend/src/components/user/atoms/TableOfContentsItem/TableOfContentsItem.tsx
+++ frontend/src/components/user/atoms/TableOfContentsItem/TableOfContentsItem.tsx
@@ -1,15 +1,26 @@
-// @ts-nocheck
+//@ts-nocheck
+
 import React from "react";
 import "./TableOfContentsItem.scss";
 
 const TableOfContentsItem = ({ icon, text, href }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const targetElement = document.getElementById(href.substring(1));
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="table-of-contents-item">
       <div className="table-of-contents-item-image">
         <img src={icon} alt="" />
       </div>
       <li>
-        <a href={href}>{text}</a>
+        <a href={href} onClick={handleClick}>
+          {text}
+        </a>
       </li>
     </div>
   );

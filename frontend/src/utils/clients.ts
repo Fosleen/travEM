4705bfdef8203df74194b84da4771440af455280
Ana Škarica <ana.skarica.as@gmail.com>
2025-02-13 19:02:20 +0100

    Add more sponsors

--- frontend/src/utils/clients.ts
+++ frontend/src/utils/clients.ts
@@ -50,4 +50,25 @@ export const clients = [
     img: CCUSA,
     url: "https://ccusa.hr/hr/",
   },
+  {
+    img: Fosleen,
+    url: "https://www.fosleen.com/",
+  },
+  {
+    img: Saily,
+    url: "https://go.saily.site/aff_c?offer_id=101&aff_id=7336",
+  },
+  {
+    img: DeraHe,
+    url: "https://www.instagram.com/deratockahe/",
+  },
+  {
+    img: NordVPN,
+    url: "https://go.nordvpn.net/aff_c?offer_id=15&aff_id=117007&url_id=902",
+  },
+  { img: Istanbulepass, url: "https://istanbulepass.com/?ref=travem" },
+  {
+    img: CCUSA,
+    url: "https://ccusa.hr/hr/",
+  },
 ];

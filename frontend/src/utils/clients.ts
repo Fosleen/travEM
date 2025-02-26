0c7d863b51ff69351383272e846131e26468814f
Ana Škarica <ana.skarica.as@gmail.com>
2025-02-26 18:29:30 +0100

    Swap NordVPN with Surfshark sponsor logo

--- frontend/src/utils/clients.ts
+++ frontend/src/utils/clients.ts
@@ -1,9 +1,9 @@
 import Fosleen from "../assets/images/logo-light.png";
 import DeraHe from "../assets/images/sponsors/DeraHe.png";
 import Saily from "../assets/images/sponsors/Saily_.png";
-import NordVPN from "../assets/images/sponsors/NordVPN.png";
 import Istanbulepass from "../assets/images/sponsors/Istanbulepass.png";
 import CCUSA from "../assets/images/sponsors/CCUSA.png";
+import Surfshark from "../assets/images/sponsors/surfshark.png";
 
 export const clients = [
   {
@@ -19,8 +19,8 @@ export const clients = [
     url: "https://www.instagram.com/deratockahe/",
   },
   {
-    img: NordVPN,
-    url: "https://go.nordvpn.net/aff_c?offer_id=15&aff_id=117007&url_id=902",
+    img: Surfshark,
+    url: "https://get.surfshark.net/aff_c?offer_id=926&aff_id=36878",
   },
   { img: Istanbulepass, url: "https://istanbulepass.com/?ref=travem" },
   {

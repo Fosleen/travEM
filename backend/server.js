ae70788e44220748b5fc120e353e0a7056c57bb6
Martin Friščić <martin.friscic6@gmail.com>
2025-04-13 19:10:47 +0200

    Fix cors again

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 createAssociations();
 
 const corsOptions = {
-  origin: "http://putujemstravem.com", //change this to localhost:5173 for testing
+  origin: "https://putujemstravem.com", //change this to localhost:5173 for testing
   credentials: true,
   methods: "GET, POST, PATCH, DELETE, PUT",
   allowedHeaders: "Content-Type, Authorization",

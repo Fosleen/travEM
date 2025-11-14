488dc374404ea634f796a99c17a129742e8a7639
Ana Škarica <ana.skarica.as@gmail.com>
2025-11-14 10:51:35 +0100

    feat: improve Croatian grammar

--- frontend/src/components/user/molecules/PopUp/PopUp.tsx
+++ frontend/src/components/user/molecules/PopUp/PopUp.tsx
@@ -36,7 +36,7 @@ const PopUp = () => {
     try {
       await addSubscriber(email);
       notifyInfo(
-        "Uspješno ste se pretplatili na newsletter! Ako ne vidite poruke, provjerite neželjenu poštu (spam mail) i maknite naš mail od tamo. Hvala."
+        "Uspješno ste se pretplatili na newsletter! Ako ne vidite poruke, provjerite neželjenu poštu (spam mail) i maknite naš mail odande. Hvala."
       );
       setEmail("");
       closePopup();

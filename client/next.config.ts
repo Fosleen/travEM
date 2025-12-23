b898c5a200682c6d52339fe5bda0cef7157d7af9
Ana Škarica <ana.skarica.as@gmail.com>
2025-12-23 14:53:05 +0100

    feat: allow new Supabase bucket images

--- client/next.config.ts
+++ client/next.config.ts
@@ -20,6 +20,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "divovzeyblkexoqlwiqy.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 };

7a1a5c993fc2c29aaaedd22eaedcc22771c04fc2
Matija Dokmanovic <travem.hr@gmail.com>
2026-05-05 11:23:53 +0200

    Fixed routes - wasn't showing forecast for the places

--- backend/app/routes/index.js
+++ backend/app/routes/index.js
@@ -22,6 +22,7 @@ import video from "./videoRoute.js";
 import airportCity from "./airportCityRoute.js";
 import { verifyToken } from "../middleware/jwt_verify.js";
 import subscriber from "./subscriberRoute.js";
+import placeBestTimeToVisit from "./placeBestTimeToVisitRoute.js";
 import countryBestTimeToVisit from "./countryBestTimeToVisitRoute.js";
 
 const router = new Router();
@@ -50,6 +51,8 @@ router.use("/continents", continent);
 router.use("/videos", video);
 router.use("/airport-cities", airportCity);
 router.use("/subscribers", subscriber);
+
+router.use("/place-best-time-to-visit", placeBestTimeToVisit);
 router.use("/country-best-time-to-visit", countryBestTimeToVisit);
 
 export default router;

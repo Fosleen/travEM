796374829712eedd7f89a99c737daed8f41c3434
Matija Dokmanovic <travem.hr@gmail.com>
2026-06-29 09:57:17 +0200

    Changed author name to be "putujEM s travEM" on ArticleHero wherever Dox or Ema writtes an article

--- client/components/user/molecules/ArticleHero/ArticleHero.tsx
+++ client/components/user/molecules/ArticleHero/ArticleHero.tsx
@@ -5,6 +5,11 @@ import ArticleImage from "../../atoms/ArticleImage";
 import "./ArticleHero.scss";
 
 const ArticleHero = ({ article }) => {
+  const authorName =
+    article.user?.username === "Dox" || article.user?.username === "Ema"
+      ? "putujEM s travEM - Ema i Matija"
+      : `${article.user?.first_name || ""} ${article.user?.last_name || ""}`.trim();
+
   return (
     <div className="article-hero-container">
       <div className="article-hero-left">
@@ -24,8 +29,7 @@ const ArticleHero = ({ article }) => {
           </h5>
 
           <h5>
-            <span>Piše:</span> {article.user?.first_name}{" "}
-            {article.user?.last_name}
+            <span>Piše:</span> {authorName}
           </h5>
         </div>
 
